334cfc304e533094daf08431070c2e38657c39a3
NuvinduSenarathne <nuvindusanjana8@gmail.com>
2023-12-16 23:54:11 +0530

    Customer List UI search, filter, pdf  exporter, csv exporter commit

--- src/services/services.customer.js
+++ src/services/services.customer.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 const apiUrl = 'http://localhost:8081/customer';
 
 const customerServices = {
+  
   createCustomer: async (data) => {
     try {
       const response = await axios.post(`${apiUrl}/create`, data, {
@@ -14,6 +15,34 @@ const customerServices = {
       throw new Error('Error creating customer');
     }
   },
+
+  getAllCustomers: async () => {
+    try {
+      const response = await axios.get(`${apiUrl}`);
+      return response.data;
+    } catch (error) {
+      throw new Error('Error fetching customers');
+    }
+  },
+
+  getCustomerById: async (customerId) => {
+    try {
+      const response = await axios.get(`${apiUrl}/${customerId}`);
+      return response.data;
+    } catch (error) {
+      throw new Error('Error fetching customer by ID');
+    }
+  },
+
+  deleteCustomer: async (customerId) => {
+    try {
+      const response = await axios.delete(`${apiUrl}/${customerId}`);
+      return response.data;
+    } catch (error) {
+      throw new Error(`Error deleting customer: ${error.message}`);
+    }
+  },
+
 };
 
 export default customerServices;
