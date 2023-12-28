cf2d2e3a8c060854eb8eff6e73e4c77187471479
Jeewanthaherath <jeewanthaherath98@gmail.com>
2023-12-28 16:30:31 +0530

    Supplier front end issue fix

--- src/components/content.supplierMaster.js
+++ src/components/content.supplierMaster.js
@@ -10,7 +10,6 @@ import { useNavigate } from "react-router-dom";
 function SupplierMaster() {
 
   const navigate = useNavigate();
-
   const [formData, setFormData] = useState({
     Title: '',
     Fullname:'',
