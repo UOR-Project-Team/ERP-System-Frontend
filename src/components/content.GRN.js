ac65589aca5b0abf859ce13295d9102f99d90d9f
Riyas <riyas.afm@gmail.com>
2024-01-08 21:18:12 +0530

    Grn List

--- src/components/content.GRN.js
+++ src/components/content.GRN.js
@@ -3,6 +3,11 @@ import Select from "react-select";
 import grnServices from '../services/services.grn';
 import DeleteLogo from './../assets/icons/delete.png';
 
+import { useUser } from '../services/services.UserContext';
+import { showErrorToast, showSuccessToast } from "../services/services.toasterMessage";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function GRN() {
 
   const [grnNumber, setgrnNumber] = useState(0);
@@ -19,6 +24,36 @@ function GRN() {
   const [discount, setDiscount] = useState(0);
   const [totalAmount, setTotalAmount] = useState(0);
 
+  const [sellingPrice, setsellingPrice] = useState('');
+  const [selectedLabel, setSelectedLabel] = useState(null);
+  const [selecteditemLabel, setSelecteditemLabel] = useState(null);
+
+  const {userid } = useUser();
+
+  const [grnData, setgrnData] = useState({
+    grnNo: '',
+    supplierid:'',
+    userid: userid,
+    puchaseditems :[],
+    totalAmount: ''
+
+  })
+
+  const [purchasedProduct, setpurchasedProduct] = useState({
+    productId: '',
+    barcode: '',
+    purchase_price: '',
+    unitprice: '',
+    quantity: ''
+  })
+
+  const handleAddItemToInvoice = (itemData) => {
+    setgrnData(prevState => ({
+      ...prevState,
+      puchaseditems: [...prevState.puchaseditems, itemData]
+    }));
+  };
+
 
   useEffect(() => {
     generateRandomNumber();
@@ -27,6 +62,10 @@ function GRN() {
   useEffect(() => {
     fetchSuppliers();
   }, []);
+
+  useEffect(()=>{
+    fetchItems();
+  },[selectedSupplierId])
 
   const fetchSuppliers = async () => {
     try {
@@ -49,11 +88,28 @@ function GRN() {
   const handlePurchasePriceChange = (event) => {
     const newValue = event.target.value;
     setPurchasePrice(newValue);
+    setpurchasedProduct(prevData =>({
+      ...prevData,
+      purchase_price: newValue,
+    }))
   };
+
+  const handleSellingPriceChange = (event) =>{
+    const newValue = event.target.value;
+    setsellingPrice(newValue);
+    setpurchasedProduct(prevData =>({
+      ...prevData,
+      unitprice: newValue,
+    }))
+  }
 
   const handleQunatityChange = (event) => {
     const newValue = event.target.value;
     setQuantity(newValue);
+    setpurchasedProduct(prevData =>({
+      ...prevData,
+      quantity: newValue,
+    }))
   };
 
   const handleDiscountChange = (event) => {
@@ -67,12 +123,28 @@ function GRN() {
     const newItem = {
       itemId: selectedItemId,
       purchasePrice: purchasePrice,
-      quantity: quantity
+      quantity: quantity,
+      sell_Price: sellingPrice
     }
     const total = calcSubtotal() + (purchasePrice * quantity);
     setSubTotal(total);
     calcTotal(total, discount);
     setSelectedItems((prevItems) => [...prevItems, newItem]);
+
+    handleAddItemToInvoice(purchasedProduct);
+    setpurchasedProduct({
+      productId: '',
+      barcode: '',
+      purchase_price: '',
+      unitprice: '',
+      quantity: '',
+    })
+
+    setQuantity('');
+    setPurchasePrice('');
+    setsellingPrice('');
+    setSelecteditemLabel(null);
+
   }
 
   const handleRemoveItem = (index, purchasePrice, quantity) => {
@@ -81,6 +153,22 @@ function GRN() {
       updatedItems.splice(index, 1);
       return updatedItems;
     });
+
+    setgrnData((prevInvoiceData) => {
+      const updatedSoldItems = [...prevInvoiceData.puchaseditems];
+      if (index >= 0 && index < updatedSoldItems.length) {
+        updatedSoldItems.splice(index, 1); // Remove item at indexToRemove
+      } else {
+        console.error('Invalid index provided');
+      }
+  
+      return {
+        ...prevInvoiceData,
+        puchaseditems: updatedSoldItems, // Update solditems array in invoiceData
+      };
+    });
+
+
     const total = calcSubtotal() - (purchasePrice * quantity);
     setSubTotal(total);
     calcTotal(total, discount);
@@ -89,6 +177,16 @@ function GRN() {
   function generateRandomNumber() {
     const randomNumber = Math.floor(1000000 + Math.random() * 9000000);
     setgrnNumber('G' + randomNumber.toString());
+    setgrnData(prevData =>({
+      ...prevData,
+      grnNo: 'G' + randomNumber.toString(),
+    }))
+  }
+
+  const BarcodeGenerator = (grnNumber,selectedproductId)=>{
+
+    const barcode = `${grnNumber}${selectedproductId}`
+    return barcode
   }
 
   function calcSubtotal() {
@@ -103,14 +201,102 @@ function GRN() {
   function calcTotal(total, discount) {
     if(discount<=0) {
       setTotalAmount(total)
+      setgrnData(prevData =>({
+        ...prevData,
+        totalAmount: total,
+      }))
     } else if(discount>0 && discount<=100) {
       setTotalAmount(total * ((100 - discount)/100))
+      setgrnData(prevData =>({
+        ...prevData,
+        totalAmount: total * ((100 - discount)/100),
+      }))
     } else if(discount>100) {
       setTotalAmount(total * (0))
     }
 
 
   }
+
+  const handlesavegrn = async(event)=>{
+    event.preventDefault();
+
+    const total = calcSubtotal() + (purchasePrice * quantity);
+    setSubTotal(total);
+    calcTotal(total, discount);
+
+    console.log(grnData);
+
+    if (
+      grnData.grnNo.trim() === '' ||
+      String(grnData.totalAmount).trim() === '' ||
+      grnData.puchaseditems.length === 0 
+    ) {
+      showErrorToast('Please fill in all required fields');
+      return;
+    }
+
+    try{
+
+      const grn = await grnServices.addgrn(grnData);
+      
+      if(grn.status === 201){
+        showSuccessToast("Updated Succesfully")
+
+        setgrnData({
+          grnNo: '',
+          supplierid:'',
+          userid: userid,
+          puchaseditems :[],
+          totalAmount: ''
+
+        })
+
+        reset();
+      }
+    }catch(error){
+      showErrorToast(" Please Try again ")
+    }
+  }
+
+  const reset = ()=>{
+
+    setSelectedSupplierId('');
+    setSelectedSupplierEmail('');
+    setSelectedSupplierMobile('');
+    setSelectedItemId('');
+    setQuantity('');
+    setPurchasePrice('');
+    setSelectedItems([])
+    setSubTotal('');
+    setDiscount('');
+    setTotalAmount('');
+    setsellingPrice('');
+    setSupplierItems([]);
+    setSuppliers([])
+
+    setgrnData({
+      grnNo: '',
+      supplierid:'',
+      userid: userid,
+      puchaseditems :[],
+      totalAmount: ''
+
+    })
+
+    generateRandomNumber();
+    setSelectedLabel(null);
+    setSelecteditemLabel(null);
+
+    //refetching 
+    fetchSuppliers();
+   // fetchItems()
+
+  }
+  const handlecancel = ()=>{
+    reset();
+  }
+
 
 
   return (
@@ -157,22 +343,29 @@ function GRN() {
                       primary: 'grey',
                     },
                   })}
+                 
                   options={suppliers.map((supplier) => ({
                     id: `${supplier.ID}`,
                     contactNo: `${supplier.ContactNo}`,
                     email: `${supplier.Email}`,
                     value: `${supplier.ID} ${supplier.Fullname} ${supplier.ContactNo} ${supplier.Email}`,
-                    label: `${supplier.Fullname}`,
+                    label: supplier.Fullname,
                   }))}
                   placeholder="Insert supplier detail"
+                  value={selectedLabel ? { label: selectedLabel } : null}
                   onChange={(selectedOption) => {
+                    setSelectedLabel(selectedOption ? selectedOption.label : null)
                     const selectedSupplierId = selectedOption ? selectedOption.id : null;
                     const selectedSupplierMobile = selectedOption ? selectedOption.contactNo : null;
                     const selectedSupplierEmail = selectedOption ? selectedOption.email : null;
                     setSelectedSupplierId(selectedSupplierId);
                     setSelectedSupplierMobile(selectedSupplierMobile);
                     setSelectedSupplierEmail(selectedSupplierEmail);
-                    fetchItems();
+                    setgrnData(prevData =>({
+                      ...prevData,
+                      supplierid: selectedSupplierId,
+                    }))
+                    
                   }}
                 />
               </span>
@@ -238,12 +431,23 @@ function GRN() {
                   options={items.map((item) => ({
                     id: `${item.ID}`,
                     value: `${item.ID}`,
-                    label: `${item.Name}`,
+                    label: item.Name,
                   }))}
+                  value={selecteditemLabel ? { label: selecteditemLabel } : null}
                   placeholder="Insert item detail"
                   onChange={(selectedOption) => {
+                    setSelecteditemLabel(selectedOption ? selectedOption.label : null)
                     const selectedIemId = selectedOption ? selectedOption.id : null;
                     setSelectedItemId(selectedIemId);
+                    setpurchasedProduct(prevData =>({
+                      ...prevData,
+                      productId: selectedIemId,
+                    }))
+                    const itembarcode = BarcodeGenerator(grnNumber,selectedIemId)
+                    setpurchasedProduct(prevData =>({
+                      ...prevData,
+                      barcode: itembarcode,
+                    }))
                   }}
                 />
               </span>
@@ -257,6 +461,10 @@ function GRN() {
             <div className='content1-container'>
               <span className='content-left'>Purchase price :</span>
               <span className='content-right'><input type='number' name='grnNo' value={purchasePrice} onChange={handlePurchasePriceChange} placeholder='xxxxx.xx' /></span>
+            </div>
+            <div className='content1-container'>
+              <span className='content-left'>Selling price :</span>
+              <span className='content-right'><input type='number' name='grnNo' value={sellingPrice} onChange={handleSellingPriceChange} placeholder='xxxxx.xx' /></span>
             </div>
             <div className='content1-container'>
               <span className='content-left'>
@@ -273,6 +481,7 @@ function GRN() {
               <th>Item Code</th>
               <th>Item Name</th>
               <th>Purchase Price</th>
+              <th>Selling Price</th>
               <th>Quantiy</th>
               <th>Total Price</th>
               <th className='action'></th>
@@ -288,7 +497,7 @@ function GRN() {
                   <tr key={selectedItem.index}>
                     <td>{index + 1}</td>
                     {items.map((item) => {
-                      if (item.ID === selectedItem.itemId) {
+                      if (item.ID == selectedItem.itemId) {
                         return (
                           <React.Fragment key={item.ID}>
                             <td>{item.Code}</td>
@@ -299,6 +508,7 @@ function GRN() {
                       return null;
                     })}
                     <td className='price'>{'Rs ' + selectedItem.purchasePrice}</td>
+                    <td className='price'>{'Rs ' + selectedItem.sell_Price}</td>
                     <td className='quantity'>{selectedItem.quantity}</td>
                     <td className='price'>{'Rs ' + selectedItem.quantity * selectedItem.purchasePrice}</td>
                     <td>
@@ -330,11 +540,12 @@ function GRN() {
       </div>
       <div className='grn-content' style={{ display: 'flex', justifyContent: 'flex-end' }}>
         <div className='button-container' >
-          <button style={{ backgroundColor: 'crimson' }}>Cancel</button>
+          <button style={{ backgroundColor: 'crimson' }} onClick={handlecancel}>Cancel</button>
           <button style={{ backgroundColor: '#2a6592' }}>Save & Print</button>
-          <button style={{ backgroundColor: 'green' }}>Save</button>
+          <button style={{ backgroundColor: 'green' }} onClick={handlesavegrn}>Save</button>
         </div>
       </div>
+      <ToastContainer/>
     </div>
   );
 }
