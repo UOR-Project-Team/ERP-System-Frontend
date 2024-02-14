b88bd6814dce27f1a28ce86d6048afc0bda6a145
Fahim <fahimbm105@gmail.com>
2024-02-14 18:44:25 +0530

    basic structure

--- src/components/content.profitLossReports.js
+++ src/components/content.profitLossReports.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function ProfitLossReports() {
 
+  const data ={
+    "income": [
+      { "category": "Salary", "amount": 5000 },
+      { "category": "Freelance Income", "amount": 2000 },
+      { "category": "Investment Income", "amount": 1000 }
+    ],
+    "expenses": [
+      { "category": "Rent", "amount": 1500 },
+      { "category": "Utilities", "amount": 200 },
+      { "category": "Groceries", "amount": 300 },
+      { "category": "Transportation", "amount": 150 }
+    ]
+  }
+
   return (
     <div>
-      Profit & Loss reports
+      <table>
+        <table>
+              <thead>
+                <tr>
+                  <th>Income</th>
+                  <th>$</th>
+                  <th className='action-column'></th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.income.map((item, index) => (
+                  <tr key={`income-${index}`}>
+                    <td>{item.category}</td>
+                    <td>{item.amount}</td>
+                  </tr>
+                ))}
+                <tr>
+                  <td>Expenses</td>
+                  <td></td> {/* Leave this column empty for expenses */}
+                </tr>
+                {data.expenses.map((item, index) => (
+                  <tr key={`expense-${index}`}>
+                    <td>{item.category}</td>
+                    <td>{item.amount}</td>
+                </tr>
+                ))}
+              </tbody>
+            </table>
+      </table>
     </div>
   );
 }
