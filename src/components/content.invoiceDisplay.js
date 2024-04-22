ea4febdd6a192b23d772c2bdc3f6d504c29c4147
Avishka <avishkaidnuvara@gmail.com>
2024-04-22 23:13:29 +0530

    Update invoice display

--- src/components/content.invoiceDisplay.js
+++ src/components/content.invoiceDisplay.js
@@ -1,4 +1,14 @@
-import React, { useState, useEffect} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import Select from "react-select";
+import grnServices from '../services/services.grn';
+import DeleteLogo from './../assets/icons/delete.png';
+import { useUser } from '../services/services.UserContext';
+import { showErrorToast, showSuccessToast } from "../services/services.toasterMessage";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import CompanyLogo from './../assets/logos/Uni_Mart.png';
+import {generatePDFGRN} from "../services/generatePrint";
+
 
 import {
   Dialog,
@@ -15,8 +25,6 @@ import Modal from 'react-modal';
 import "jspdf-autotable";
 import jsPDF from 'jspdf';
 import Papa from 'papaparse';
-import { ToastContainer } from 'react-toastify';
-import { showSuccessToast, showErrorToast } from '../services/services.toasterMessage';
 import AddLogo from './../assets/icons/add.png';
 import PdfLogo from './../assets/icons/pdf.png';
 import CsvLogo from './../assets/icons/csv.png';
@@ -24,11 +32,10 @@ import FilterLogo from './../assets/icons/filter.png';
 import SearchLogo from './../assets/icons/search.png';
 import ViewLogo from './../assets/icons/view.png';
 import ActionLogo from './../assets/icons/action.png';
-import DeleteLogo from './../assets/icons/delete.png';
 import invoiceServices from '../services/services.invoice';
 
 
-function InvoiceList() {
+function InvoiceDisplay() {
 
   const [Invoices, setInvoices] = useState([]);
   const [anchorEl, setAnchorEl] = useState(null);
@@ -45,136 +52,15 @@ function InvoiceList() {
   
 
 
-  useEffect(()=>{
-    fetchInvoices();
-  }, []);
+  const handleClick = (event, invoiceId) => {
+    // Logic to handle click event
+    console.log('Button clicked for invoice ID:', invoiceId);
+    // You can add more logic here as needed
+  };
+
+
   
-
-
-
-
-
-  //fetch all invoices function
-  const fetchInvoices = async ()=>{
-    try{
-      const invoiceData= await invoiceServices.getAllInvoices();
-      console.log(invoiceData)
-      //setItems(itemData);
-      setInvoices([...invoiceData]);
-    }
-    catch(error)
-    {
-      console.error('Error fetching invoices',error.message);
-    }
-  }
-
-
-  const handleDialogAction = async () => {
-    if(dialogTitle === 'PDF Exporter') {
-      exportPDF();
-    } else if(dialogTitle === 'CSV Exporter') {
-      exportCSV();
-    } else if(dialogTitle === 'Delete Item') {
-      
-      try {
-        await invoiceServices.deleteInvoice(currentInvoice);
-        fetchInvoices();
-        setDialogOpen(false);
-        showSuccessToast('Invoice successfully deleted');
-
-        } catch (error) {
-            console.error('Error deleting invoice:', error.message);
-            showErrorToast('Error deleting invoice')
-          }
-        }
-      };
-
-
-      const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-      };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-      };
-
-    const handleRequest = (type) => {
-        navigateTo(`/home/invoice-display`)
-        
-      };
-
-
-      const exportPDF = () => {
-        const unit = "pt";
-        const size = "A4";
-        const orientation = "landscape";
-        const doc = new jsPDF(orientation, unit, size);
-
-      const header = function(data) {
-            doc.setFontSize(8);
-            doc.setTextColor(40);
-            doc.text("Innova ERP Solution - Item Report", data.settings.margin.left, 30);
-      };
-
-      const footer = function(data) {
-        const pageCount = doc.internal.getNumberOfPages();
-        doc.text("Page " + data.pageNumber + " of " + pageCount, data.settings.margin.left, doc.internal.pageSize.height - 10);
-    };
-
-    const headers = [["ID", "No","Date/Time","Total Amount","User","Customer"]];
-
-   
-    const data = Invoices.map(elt=> [elt.ID, elt.No, elt.Date_Time , elt.Total_Amount, elt.UserName,elt.CustomerName]);
-
-    
-    let content = {
-      startY: 50,
-      head: headers,
-      body: data
-    };
-
-    doc.autoTable({
-        ...content,
-        theme: 'striped',
-        headerStyles: { fillColor: [38, 2, 97], textColor: [255, 255, 255] },
-        addPageContent: function(data) {
-            header(data);
-            footer(data);
-        }
-      });
   
-      setDialogOpen(false);
-      doc.save("ERP-item-report.pdf");
-    };
-
-
-    const exportCSV = () => {
-      const headers = ["ID", "No","Date/Time","Total Amount","User","Customer"];
-    
-      const data = Invoices.map(elt => [
-        elt.ID,
-        elt.No,
-        elt.Date_Time ,
-        elt.Total_Amount, 
-        elt.UserName,
-        elt.CustomerName
-      ]);
-    
-      const csvData = [headers, ...data];
-    
-      const csv = Papa.unparse(csvData);
-    
-      const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
-    
-      const link = document.createElement('a');
-      const url = URL.createObjectURL(blob);
-      link.href = url;
-      link.setAttribute('download', 'ERP-Item-report.csv');
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-      setDialogOpen(false);
-    };
 
 
     
@@ -201,7 +87,7 @@ function InvoiceList() {
   
       if (searchInput === '') {
         
-        await fetchInvoices();
+        
   
       } else {
         const res = await invoiceServices.getAllInvoices();
@@ -215,6 +101,7 @@ function InvoiceList() {
       console.error('Error handling search input',error.message)
     }
     };
+    
 
 
         
@@ -244,12 +131,15 @@ function InvoiceList() {
         </div>
       </div>
 
-      <div>
+      <div className='table-container'>
 
       <label>
-        <script>
-          
-        </script>
+        <table>
+          <tr>
+          <td width='700px'><br/>refg</td>
+          <td>tfgh</td>
+          </tr>
+        </table>
       </label>
 
       </div>
@@ -301,32 +191,24 @@ function InvoiceList() {
               )}
             </tbody>
           </table>
-          <br/>
-
-          <div>
           
-          </div>
-        </div>
-      </div>
-      </div>
-      
 
-      <Dialog open={removeClick} onClose={() => setDialogOpen(false)} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
-              <DialogTitle id="alert-dialog-title">{dialogTitle}</DialogTitle>
-              <DialogContent>
-              <DialogContentText id="alert-dialog-description" style={{width: '250px'}}>
-              {dialogDescription}
-              </DialogContentText>
-              </DialogContent>
-              <DialogActions>
-              <Button color="primary" onClick={handleDialogAction}>
-              Yes
-              </Button>
-              <Button color="primary" autoFocus onClick={() => setDialogOpen(false)}>
-              No
-              </Button>
-              </DialogActions>
-      </Dialog>
+          
+        </div>
+        
+      
+      </div>
+      </div>
+      <div className='grn-content' style={{ display: 'flex', justifyContent: 'flex-end' }}>
+  <div className='button-container button' >
+    <button style={{ backgroundColor: '#2a6592', marginRight: '10px', borderRadius: '5px', padding: '5px 20px' }}>Print</button>
+    <button style={{ backgroundColor: 'green', borderRadius: '5px', padding: '5px 20px' }}>Ok</button>
+  </div>
+</div>
+
+
+
+      
 
       
 
@@ -336,4 +218,4 @@ function InvoiceList() {
   );
 }
 
-export default InvoiceList;
+export default InvoiceDisplay;
